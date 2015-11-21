5dce195e951e411c11873a340cff5983a61d2915
Marcelo Cenerino <marcelocenerine@gmail.com>
2015-11-21 20:45:46 +0000

    move files to submodule and configure session replication with redis

--- session-repl-web/src/main/scala/com/cenerino/sessionrepl/Configuration.scala
+++ session-repl-web/src/main/scala/com/cenerino/sessionrepl/Configuration.scala
@@ -2,8 +2,10 @@ package com.cenerino.sessionrepl
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
 
 @Configuration
 @EnableAutoConfiguration
+@EnableRedisHttpSession
 @ComponentScan
 class Configuration
